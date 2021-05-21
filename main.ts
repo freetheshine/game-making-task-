9426fdd0405cec423a50954e0b996d9263981682
freetheshine <83438528+freetheshine@users.noreply.github.com>
2021-05-21 12:32:38 +1000

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const COIN = SpriteKind.create()
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (JACOB.vy == 0) {
         JACOB.vy = -150
@@ -9,6 +12,11 @@ scene.onOverlapTile(SpriteKind.Player, assets.tile`portal`, function (sprite, lo
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
     game.over(false, effects.melt)
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.COIN, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    otherSprite.destroy()
+})
+let COIN: Sprite = null
 let JACOB: Sprite = null
 scene.setBackgroundColor(9)
 JACOB = sprites.create(assets.image`JACOB`, SpriteKind.Player)
@@ -16,3 +24,24 @@ controller.moveSprite(JACOB, 80, 100)
 tiles.setTilemap(tilemap`level4`)
 JACOB.ay += 200
 scene.cameraFollowSprite(JACOB)
+for (let value of tiles.getTilesByType(assets.tile`myTile4`)) {
+    COIN = sprites.create(img`
+        . . . f f f f f f f . . . . . . 
+        . . f 5 5 5 5 5 5 5 f . . . . . 
+        . f 5 5 4 4 4 4 4 4 5 f . . . . 
+        f 5 5 5 5 5 5 5 5 5 5 5 f . . . 
+        f 5 5 5 5 5 5 5 5 5 5 5 f . . . 
+        f 5 4 5 5 5 5 5 5 5 5 5 f . . . 
+        f 5 5 5 5 5 5 5 5 4 5 5 f . . . 
+        f 5 5 5 4 4 5 5 5 4 5 5 f . . . 
+        f 5 5 5 5 5 5 5 5 4 5 5 f . . . 
+        f 5 5 5 4 4 5 5 5 5 5 5 f . . . 
+        . f 5 5 5 5 5 5 5 5 5 f . . . . 
+        . . f 5 5 5 5 5 5 5 f . . . . . 
+        . . . f f f f f f f . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.COIN)
+    tiles.placeOnTile(COIN, value)
+}
